e8004949f44092c584444f1f6b628a75f13189e2
ivanherve <ivanherve1411@gmail.com>
2019-03-24 16:29:41 +0200

    New links & components added

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -6,6 +6,9 @@ import MyAdvertisments from '../views/Containers/myAdvertisments';
 import VisitsList from '../views/Containers/visitsList';
 import Footer from './Footer';
 import Headers from './Header';
+import ResetPwd from '../views/Containers/resetpwd';
+import Contact from '../views/Containers/contact';
+import Profile from '../views/Containers/profile';
 
 
 class Layout extends Component {
@@ -28,17 +31,20 @@ class Layout extends Component {
     return (
       <div>
         <Headers onClick={() => this.setState({ redirect: true })} />
-        <div style={{ marginTop: '70px' }}>
+        <div style={{ marginTop: '70px', marginBottom: '50px' }}>
           {
-            sessionStorage.getItem('userData')
-              ?
+            sessionStorage.getItem('userData') 
+              ? // If signed In
               <Switch>
                 <Route path='/accomodations' component={AccomodationsList} />
                 <Route path='/annonces' component={MyAdvertisments} />
                 <Route path='/visites' component={VisitsList} />
+                <Route path='/resetpwd' component={ResetPwd} />
+                <Route path='/contact' component={Contact} />
+                <Route path='/profile' component={Profile} />
                 <Redirect from="/" to="/accomodations" />
               </Switch>
-              :
+              : // If signed Out
               <Switch>
                 <Route path='/accomodations' component={AccomodationsList} />
                 <Redirect from="/" to="/accomodations" />
