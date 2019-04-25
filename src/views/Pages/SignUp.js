d6de75399f34abffbefae088c8fae71632573582
ivanherve <ivanherve1411@gmail.com>
2019-04-25 15:56:24 +0200

    signing up problem solved

--- src/views/Pages/SignUp.js
+++ src/views/Pages/SignUp.js
@@ -16,16 +16,16 @@ export default class SignUp extends Component {
     }
 
     signIn = (email, password) => {
+        let data = new FormData();
+        data.append('email', email);
+        data.append('password', password);
         fetch(apiUrl + 'signin', {
             method: 'POST',
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
+            body: data
         })
             .then(response => response.json())
             .then(res => {
-                if (res.status == 'success') {
+                if (res.status === 'success') {
                     sessionStorage.setItem('userData', JSON.stringify(res.response));
                     this.props.redirection();
                 }
@@ -53,6 +53,7 @@ export default class SignUp extends Component {
                 .then(res => {
                     if (res.status === "success") {
                         this.signIn(res.response.email, this.state.password)
+                        console.log(res.response)
                     }
                     else {
                         alert(res.response);
@@ -63,32 +64,28 @@ export default class SignUp extends Component {
 
     }
 
-    handleField(e) {
-        this.setState({[e.target.name]:e.target.value});
-        console.log(e.target.name,e.target.value)
-    }
-
-    handlePassword(p) {
-        this.setState({ password: p.target.value })
-    }
-
     render() {
         return (
             <Card.Body className="text-center">
                 <p>Cherchez, Annoncez et organisez des visites de la plus simple des façons</p>
                 <Form>
-                    <input className="form-control" placeholder="Prénom*" name="firstname" type="text" required onChange={this.handleField} />
+                    <Form.Control placeholder="Prénom*" name="firstname" type="text" required onChange={e => this.setState({ firstname: e.target.value })} />
                     <br />
-                    <input className="form-control" placeholder="Nom*" name="surname" type="text" required onChange={this.handleField} />
+                    <Form.Control placeholder="Nom*" name="surname" type="text" required onChange={e => this.setState({ surname: e.target.value })} />
                     <br />
-                    <input className="form-control" placeholder="adresse@email.com*" name="email" type="text" required onChange={this.handleField} />
+                    <Form.Control placeholder="adresse@email.com*" name="email" type="text" required onChange={e => this.setState({ email: e.target.value })} />
                     <br />
-                    <input className="form-control" placeholder="Mot de passe*" name="password" type="password" minLength="8" required onChange={this.handleField} />
+                    <Form.Control placeholder="Mot de passe*" name="password" type="password" minLength="8" required onChange={e => this.setState({ password: e.target.value })} />
                     <br />
-                    <input className="form-control" placeholder="Confirmer votre mot de passe*" name="passwordtoconfirm" minLength="8" type="password" required onChange={this.handleField} />
+                    <Form.Control placeholder="Confirmer votre mot de passe*" name="passwordtoconfirm" minLength="8" type="password" required onChange={e => this.setState({pswToConfirm: e.target.value})} />
                     <br />
-                    <input className="button mt-3" type="submit" color="primary" value="Inscrivez-vous maintenant!" name="Register" onSubmit={() => this.signUp()} />
-                        
+                    <Button color="primary" name="Register" onClick={() => {
+                        if(this.state.password !== this.state.pswToConfirm) alert('Les mots de passes ne sont pas identiques');
+                        else this.signUp();
+                    }}>
+                        Inscrivez-vous maintenant!
+                    </Button>
+
                 </Form>
             </Card.Body>
         )
