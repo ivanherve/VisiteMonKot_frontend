5536569dc61eb5dcdcf9a39405472ef206792951
ivanherve <ivanherve1411@gmail.com>
2019-04-28 17:01:21 +0200

    url of geoservices.wallonie.be's API

--- src/router.js
+++ src/router.js
@@ -1,2 +1,4 @@
 export const domain = 'localhost';
 export const apiUrl = 'http://'+domain+':8080/';
+export const API_URL_CP = 'http://geoservices.wallonie.be/geolocalisation/rest/getListeLocalitesByCp';
+export const API_URL_RUE = 'http://geoservices.wallonie.be/geolocalisation/rest/getListeRuesByLocaliteAndNomApprox/';
