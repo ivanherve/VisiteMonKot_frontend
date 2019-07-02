a83f823c8e1292eab80afa7a1423e330f51e9cf6
ivanherve <ivanherve1411@gmail.com>
2019-07-02 17:12:51 +0200

    sign out a user

--- src/views/Modals/signoutAUser.js
+++ src/views/Modals/signoutAUser.js
@@ -1,9 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import { apiUrl } from '../../router';
+import swal from 'sweetalert';
 
 export default class SignOutAUser extends Component {
-    render(){
-        return(
+
+    signout = (uid) => {
+        let data = new FormData();
+        data.append('uid', uid);
+        fetch(`${apiUrl}signoutauser`, {
+            method: 'post',
+            headers: {
+                api_token: JSON.parse(sessionStorage.getItem('userData')).token.api_token
+            },
+            body: data
+        }).then(response => response.json())
+            .then(res => {
+                if (res.status === 'error') {
+                    swal({
+                        text: res.response[0],
+                        icon: 'warning'
+                    })
+                } else {
+                    console.log(res.response);
+                    swal({
+                        text: res.response,
+                        icon: 'success',
+                    }).then(() => {
+                        this.props.hide();
+                        window.location.reload();
+                    })
+                }
+            })
+    }
+
+    render() {
+        return (
             <Modal show={this.props.show} onHide={this.props.hide} centered>
                 <Modal.Header closeButton>
                     <Modal.Title>Déconnecter {this.props.name}</Modal.Title>
@@ -12,7 +44,7 @@ export default class SignOutAUser extends Component {
                     Etes-vous sûr de vouloir déconnecter <strong>{this.props.name}</strong> ?
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant='outline-warning'>DECONNECTER</Button>
+                    <Button variant='outline-warning' onClick={() => this.signout(this.props.uid)}>DECONNECTER</Button>
                 </Modal.Footer>
             </Modal>
         )
