83584eb368386ab0849afd0c15122a6f10c6fca5
ivanherve <ivanherve1411@gmail.com>
2019-08-06 17:29:13 +0200

    recaptcha verification

--- src/views/Pages/SignUp.js
+++ src/views/Pages/SignUp.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { Button, Card, Form, Row, Col } from 'react-bootstrap';
 import { apiUrl, API_URL_TEL } from '../../router';
 import swal from 'sweetalert';
+import Recaptcha from "react-recaptcha";
 
 export default class SignUp extends Component {
     constructor() {
         super();
+        this.recaptchaLoaded = this.recaptchaLoaded.bind(this);
+        this.verifyCallback = this.verifyCallback.bind(this);
+
         this.state = {
             email: '',
             firstname: '',
@@ -15,6 +19,7 @@ export default class SignUp extends Component {
             callingCode: null,
             countries: [],
             mobile: null,
+            isHuman: false,
         }
     }
 
@@ -40,31 +45,38 @@ export default class SignUp extends Component {
     }
 
     signUp = () => {
-        if (this.state.password !== this.state.pswToConfirm) {
-            swal('Les mots de passes ne correspondent pas entre eux')
-        } else {
-            let data = new FormData();
-            data.append('email', this.state.email);
-            data.append('firstname', this.state.firstname);
-            data.append('surname', this.state.surname);
-            data.append('password', this.state.password);
-            data.append('callCode', this.state.callingCode)
-            data.append('mobile', this.state.mobile)
-            fetch(apiUrl + 'signup', {
-                method: 'POST',
-                body: data
-            })
-                .then(response => response.json())
-                .then(res => {
-                    if (res.status === "success") {
-                        this.signIn(res.response.email, this.state.password)
-                        console.log(res.response)
-                    }
-                    else {
-                        swal(res.response);
-                        console.log(res.response)
-                    }
+        if (this.state.isHuman) {
+            if (this.state.password !== this.state.pswToConfirm) {
+                swal('Les mots de passes ne correspondent pas entre eux')
+            } else {
+                let data = new FormData();
+                data.append('email', this.state.email);
+                data.append('firstname', this.state.firstname);
+                data.append('surname', this.state.surname);
+                data.append('password', this.state.password);
+                data.append('callCode', this.state.callingCode)
+                data.append('mobile', this.state.mobile)
+                fetch(apiUrl + 'signup', {
+                    method: 'POST',
+                    body: data
                 })
+                    .then(response => response.json())
+                    .then(res => {
+                        if (res.status === "success") {
+                            this.signIn(res.response.email, this.state.password)
+                            console.log(res.response)
+                        }
+                        else {
+                            swal({ text: res.response[0] });
+                            console.log(res.response)
+                        }
+                    })
+            }
+        } else {
+            swal({
+                icon: 'warning',
+                text: 'Veuillez, s\'il vous plait, confirmer que vous n\'êtes pas un robot'
+            })
         }
     }
 
@@ -89,10 +101,18 @@ export default class SignUp extends Component {
         console.log(callingCode)
     }
 
+    recaptchaLoaded() {
+        console.log('captcha loaded')
+    }
+
+    verifyCallback(response) {
+        if (response) this.setState({ isHuman: true })
+        console.log(response)
+    }
+
     componentDidMount() {
         this.getCallingCodes();
     }
-
 
     render() {
         return (
@@ -123,9 +143,20 @@ export default class SignUp extends Component {
                     <Form.Control placeholder="Mot de passe*" name="password" type="password" minLength="8" required onChange={e => this.setState({ password: e.target.value })} />
                     <br />
                     <Form.Control placeholder="Confirmer votre mot de passe*" name="passwordtoconfirm" minLength="8" type="password" required onChange={e => this.setState({ pswToConfirm: e.target.value })} />
-                    <br />{/*
-                    <Form.Check type='checkbox' custom label={<div>J'ai lu et accepté les conditions générales d'utilisation</div>} />
-                    <br />*/}
+                    <br />
+                    {
+                        /*
+                            <Form.Check type='checkbox' custom label={<div>J'ai lu et accepté les conditions générales d'utilisation</div>} />
+                            <br />
+                        */
+                    }
+                    <Recaptcha
+                        sitekey="6Ld5C7EUAAAAAEtth02r4DRaEjwNhFOvO31V96Tr"
+                        render="explicit"
+                        onloadCallback={this.recaptchaLoaded}
+                        verifyCallback={this.verifyCallback}
+                    />
+                    <br />
                     <Button color="primary" name="Register" onClick={() => {
                         if (this.state.password !== this.state.pswToConfirm) swal('Les mots de passes ne sont pas identiques');
                         else this.signUp();
