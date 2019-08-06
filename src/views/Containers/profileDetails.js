6c9022a87075d94e879685908ed6c5413b5c72be
ivanherve <ivanherve1411@gmail.com>
2019-08-06 17:20:22 +0200

    edit mobile number

--- src/views/Containers/profileDetails.js
+++ src/views/Containers/profileDetails.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 import 'moment/locale/fr';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import swal from 'sweetalert';
-import { apiUrl } from '../../router';
+import { apiUrl, API_URL_TEL } from '../../router';
 
 export default class ProfileDetails extends Component {
 
@@ -15,8 +15,39 @@ export default class ProfileDetails extends Component {
             email: '',
             Firstname: '',
             Surname: '',
+            call_code: null,
+            mobile: '',
+            codes: []
         }
     }
+
+    componentDidMount() {
+        this.getCallCodes();
+    }
+
+    getCallCodes = () => {
+        fetch(`${API_URL_TEL}`)
+            .then(res => res.json())
+            .then(res => {
+                //console.log(res)
+                res.map(el => {
+                    if (this.state.codes.length > 0) {
+                        this.setState({ codes: [...this.state.codes, { country: el.translations.fr ? el.translations.fr : el.name, code: el.callingCodes[0] }] })
+                    } else {
+                        this.setState({
+                            codes: [
+                                {
+                                    country: el.translations.fr ? el.translations.fr : el.name,
+                                    code: el.callingCodes[0]
+                                }
+                            ]
+                        })
+                    }
+                })
+            })
+    }
+
+
 
     editInformations = () => {
         if (this.state.email === "") {
@@ -28,6 +59,12 @@ export default class ProfileDetails extends Component {
         if (this.state.Surname === "") {
             this.setState({ Surname: this.props.user.Surname })
         }
+        if (this.state.call_code === "") {
+            this.setState({ call_code: this.props.user.call_code })
+        }
+        if (this.state.mobile === "") {
+            this.setState({ mobile: this.props.user.mobile })
+        }
         swal({
             text: 'Etes-vous sûr de modifier ces informations ?',
             button: {
@@ -38,6 +75,8 @@ export default class ProfileDetails extends Component {
             data.append('firstname', this.state.Firstname);
             data.append('surname', this.state.Surname);
             data.append('email', this.state.email);
+            data.append('call_code', this.state.call_code);
+            data.append('mobile', this.state.mobile);
             fetch(`${apiUrl}resetinfo`, {
                 method: 'post',
                 headers: {
@@ -59,12 +98,19 @@ export default class ProfileDetails extends Component {
                             icon: 'success'
                         }).then(() => {
                             this.setState({ toEdit: true });
+                            window.location.reload();
                             console.log({ 'email': this.state.email, "firstname": this.state.Firstname, "surname": this.state.Surname, "default-Value": this.props.user });
                         })
                     }
                 })
 
         })
+    }
+
+    handleMobile = e => {
+        let array = e.target.value.split(' ');
+        //console.log(array[1])
+        this.setState({ call_code: array[1] })
     }
 
     render() {
@@ -125,6 +171,35 @@ export default class ProfileDetails extends Component {
                     </Form.Group>
                     <Form.Group as={Row} controlId="formPlaintextEmail">
                         <Form.Label column sm="3">
+                            N° de Téléphone
+                        </Form.Label>
+                        <Col sm="9">
+                            {
+                                this.state.toEdit
+                                    ?
+                                    <Form.Control plaintext defaultValue={`+${user.call_code} ${user.mobile}`} onChange={(e) => this.setState({ mobile: e.target.value })} />
+                                    :
+                                    <Row>
+                                        <Col xs='4'>
+                                            <Form.Control as='select' defaultValue={`+${user.call_code}`} onChange={e => this.handleMobile(e)}>
+                                                {
+                                                    this.state.codes.map(c =>
+                                                        <option key={this.state.codes.indexOf(c)}>+ {c.code} - {c.country}</option>
+                                                    )
+                                                }
+                                                <option>{`+${user.call_code}`}</option>
+                                            </Form.Control>
+                                        </Col>
+                                        <Col xs='8'>
+                                            <Form.Control defaultValue={user.mobile} onChange={(e) => this.setState({ mobile: e.target.value })} />
+                                        </Col>
+                                    </Row>
+                            }
+
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="3">
                             Date d'inscription
                     </Form.Label>
                         <Col sm="9">
@@ -133,19 +208,29 @@ export default class ProfileDetails extends Component {
                     </Form.Group>
                     <Form.Group as={Row} controlId="formPlaintextEmail">
                         <Form.Label column sm="3">
-                            Compte
-                    </Form.Label>
-                        {
-                            user.isAccountActive === 1
-                                ?
-                                <Col sm="9">
-                                    <Form.Control plaintext readOnly defaultValue="Actif" style={{ color: 'green' }} />
-                                </Col>
-                                :
-                                <Col sm="9">
-                                    <Form.Control plaintext readOnly defaultValue="Inactif" style={{ color: 'red' }} />
-                                </Col>
-                        }
+                            Statut
+                        </Form.Label>
+                        <Col sm="9">
+                            {
+                                user.profil_id === 1
+                                    ?
+                                    <Form.Control plaintext readOnly defaultValue="Inactif" style={{ color: '#fcba03' }} />
+                                    :
+                                    user.profil_id === 2
+                                        ?
+                                        <Form.Control plaintext readOnly defaultValue="Actif" style={{ color: 'green' }} />
+                                        :
+                                        user.profil_id === 3
+                                            ?
+                                            <Form.Control plaintext readOnly defaultValue="Administrateur" style={{ color: 'bleu' }} />
+                                            :
+                                            user.profil_id === 5
+                                                ?
+                                                <Form.Control plaintext readOnly defaultValue="Tester" style={{ color: '#c4009a' }} />
+                                                :
+                                                <Form.Control plaintext readOnly defaultValue="Bannis" style={{ color: 'red' }} />
+                            }
+                        </Col>
                     </Form.Group>
                 </Form>
             </div>
