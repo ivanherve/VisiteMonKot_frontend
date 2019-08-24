9c9b2c8a6aacb34220efb4fbb903353056a21e93
ivanherve <ivanherve1411@gmail.com>
2019-08-24 10:34:09 +0200

    refractoring on profile page

--- src/views/Containers/profile.js
+++ src/views/Containers/profile.js
@@ -3,7 +3,7 @@ import 'moment/locale/fr';
 import React, { Component } from 'react';
 import { Card, Col, Container, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
 import { Link, Redirect, Route, Switch } from 'react-router-dom';
-import ProfilePic from '../../Pictures/testimonial-2.jpg';
+import ProfilePic from '../../logo/default-profile-picture.png';
 import ProfileDetails from './profileDetails';
 import ResetPwd from './resetpwd';
 
@@ -18,9 +18,12 @@ export default class Profile extends Component {
                         <Col xs={3}>
                             <Card>
                                 <Card.Img variant="top" src={ProfilePic} />
+                                <Card.Header>
+                                    <Card.Title>{user.Firstname} {user.Surname}</Card.Title>
+                                </Card.Header>
                                 <ListGroup className="list-group-flush">
-                                    <Link to={'/profile/'+user.user_id} className="list-group-item">Modifier mes informations personnelles</Link>
-                                    <Link to={'/profile/resetpwd/'+user.user_id} className="list-group-item">Modifier mon mot de passe</Link>
+                                    <Link to={'/profile/' + user.user_id} className="list-group-item">Modifier mes informations personnelles</Link>
+                                    <Link to={'/profile/resetpwd/' + user.user_id} className="list-group-item">Modifier mon mot de passe</Link>
                                     <ListGroupItem variant="danger" onClick={() => alert('lala')}>
                                         Supprimer mon compte
                                     </ListGroupItem>
@@ -29,9 +32,9 @@ export default class Profile extends Component {
                         </Col>
                         <Col xs={9}>
                             <Switch>
-                                <Route path={'/profile/'+user.user_id} render={() => <ProfileDetails user={user} />} />
-                                <Route path={'/profile/resetpwd/'+user.user_id} render={() => <ResetPwd />} />
-                                <Redirect from='/profile' to={'/profile/'+user.user_id} />
+                                <Route path={'/profile/' + user.user_id} render={() => <ProfileDetails user={user} />} />
+                                <Route path={'/profile/resetpwd/' + user.user_id} render={() => <ResetPwd />} />
+                                <Redirect from='/profile' to={'/profile/' + user.user_id} />
                             </Switch>
                         </Col>
                     </Row>
