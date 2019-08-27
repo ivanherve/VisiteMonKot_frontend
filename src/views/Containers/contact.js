56f29697970ecd27f402ae39d624f4d9c3871cc4
ivanherve <ivanherve1411@gmail.com>
2019-08-27 14:52:45 +0200

    send messages to admin

--- src/views/Containers/contact.js
+++ src/views/Containers/contact.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
 import { Container, Form, Button } from 'react-bootstrap';
+import { apiUrl } from '../../router';
+import swal from 'sweetalert';
 
 export default class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: '',
+        }
+    }
+
+    sendMsg = () => {
+        let data = new FormData();
+        data.append('message',this.state.message);
+        fetch(`${apiUrl}mail`, {
+            method: 'post',
+            headers: {
+                'Authorization': JSON.parse(sessionStorage.getItem('userData')).token.api_token
+            },
+            body: data
+        }).then(res => res.json())
+        .then(res => {
+            if(res.status === 'success'){
+                swal("Merci!",res.response,'success')
+                .then(() => window.location.reload())
+            } else {
+                swal("Attention",res.response[0],"warning")
+            }
+        })
+        .catch(err => {
+            swal("Oups!", "Une erreur est survenue", "error");
+            console.log(err)
+        })
+    }
+
     render() {
         return (
             <Container>
-                <div style={{marginTop: '10px'}}>
-                <h1>Contacter l'équipe VisiteMonKot</h1>
-                <Form>
-                    <Form.Control
-                        type="text"
-                        as="textarea"
-                        name="message"
-                        placeholder="Chers Administrateurs, ... "
-                        style={{minHeight: '200px'}}
-                    />
-                    <br />
-                </Form>
-                <Button variant="success">
-                    Envoyer un message
-                </Button>
+                <div style={{ marginTop: '10px' }}>
+                    <h1>Contacter l'équipe VisiteMonKot</h1>
+                    <Form>
+                        <Form.Control
+                            type="text"
+                            as="textarea"
+                            name="message"
+                            placeholder="Chers Administrateurs, ... "
+                            style={{ minHeight: '200px' }}
+                            onChange={e => this.setState({ message: e.target.value })}
+                        />
+                        <br />
+                    </Form>
+                    <Button variant="success" onClick={() => this.sendMsg()}>
+                        Envoyer un message
+                    </Button>
                 </div>
             </Container>
         )
