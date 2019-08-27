8c84c1ae5f80359abc545d3b235d0286a97ce028
ivanherve <ivanherve1411@gmail.com>
2019-08-28 00:00:25 +0200

    switched to https geoservices.wallonie

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ export const domain = 'visitemonkot.be';
 export const apiUrl = 'https://'+domain+':4431/';
 //export const domain = 'localhost';
 //export const apiUrl = 'http://'+domain+':8080/';
-export const API_URL_CP  = 'http://geoservices.wallonie.be/geolocalisation/rest/getListeLocalitesByCp';
-export const API_URL_RUE = 'http://geoservices.wallonie.be/geolocalisation/rest/getListeRuesByLocaliteAndNomApprox/';
+export const API_URL_CP  = 'https://geoservices.wallonie.be/geolocalisation/rest/getListeLocalitesByCp';
+export const API_URL_RUE = 'https://geoservices.wallonie.be/geolocalisation/rest/getListeRuesByLocaliteAndNomApprox/';
 
 export const API_URL_TEL = 'https://restcountries.eu/rest/v2/region/europe?fields=name;callingCodes;translations';
